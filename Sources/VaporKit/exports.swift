d29685756a19069f3471a8e4b1553b9ee3308ec3
Arkivili Collindort <isoblack1@outlook.com>
2026-05-31 18:07:38 +0800

    Update Vapor re-exports
    Now compiler will warn if target platform isn't available for Vapor.

--- Sources/VaporKit/exports.swift
+++ Sources/VaporKit/exports.swift
@@ -5,4 +5,8 @@
 //  Created by Arkivili Collindort on 31/05/2026
 //
 
+#if canImport(Vapor)
 @_exported import Vapor
+#else
+#error("VaporKit requires Vapor, but Vapor is unavailable on this target platform.")
+#endif
